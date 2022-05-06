df8e3995b13cf1b834e92b8f8f72d39404b4cae5
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-05-06 15:08:07 -0700

    WIP-Add new transaction

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -9,6 +9,14 @@ import {
   TableRow,
   TableCell,
 } from '@mui/material';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Create from './create';
 
 const Record = (props) => (
   <tr>
@@ -35,6 +43,11 @@ const Record = (props) => (
 
 export default function RecordList() {
   const [records, setRecords] = useState([]);
+  const[open,setOpen]=useState(false)
+  const handleAddTransaction = ()=>{
+  setOpen(true)
+
+  }
 
   // This method fetches the records from the database.
   useEffect(() => {
@@ -81,6 +94,7 @@ export default function RecordList() {
 
   // This following section will display the table with the records of individuals.
   return (
+    <>
     <Paper sx={{ width: '100%' }}>
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table sx={{ minWidth: 650 }} stickyHeader aria-label='simple table'>
@@ -118,5 +132,34 @@ export default function RecordList() {
         </Table>
       </TableContainer>
     </Paper>
+    <div>
+      <Button variant="outlined" onClick={handleAddTransaction}>
+        Add  Transaction
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          {/* <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          /> */}
+          <Create/>
+        </DialogContent>
+        {/* <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions> */}
+      </Dialog>
+    </div>
+    </>
   );
 }
